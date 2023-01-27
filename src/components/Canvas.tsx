bc5e1d514e8575c65906a0d61f9a267be3dd502d
SebastianReyes <prof.reyes.sebastian50@gmail.com>
2023-01-26 22:51:09 -0600

    orbits

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,16 +7,25 @@ function Canvas(){
         antialias: true,
         //alpha: true <- at deployment i will undo this...
     });
-    
+    var tilt = Math.PI * 23.5 / 180;    
     renderer.setSize(window.innerWidth, window.innerHeight);
     
     document.body.appendChild(renderer.domElement);
 
     var geometry = new THREE.SphereGeometry(1.5, 20, 10);
+    var little = new THREE.SphereGeometry(.05, 10, 10);
     var mat = new THREE.MeshNormalMaterial({flatShading: true});
     
     var sphere = new THREE.Mesh(geometry, mat);
+    var moon = new THREE.Mesh(little, mat);
+    moon.position.x += 3;
+
+    var axis = new THREE.Vector3(0, 1, 0).normalize();
+    var center = new THREE.Vector3(0, 0, 0);
+
     scene.add(sphere);
+    scene.add(moon);
+    
     sphere.rotation.set(0, 0, Math.PI * 23.5 / 180);
 
     camera.position.z = 5;
@@ -25,8 +34,15 @@ function Canvas(){
         setTimeout(function(){
             requestAnimationFrame(animation);
             sphere.rotateY(.1);
+            var q = new THREE.Quaternion();
+            q.setFromAxisAngle(axis, Math.PI * 2.5 / 180);
+            moon.applyQuaternion(q);
+            moon.position.sub(center);
+            moon.rotateY(.5);
+            moon.position.applyQuaternion(q);
+            moon.position.add(center);
             renderer.render(scene, camera);
-        }, 1000/2)
+        }, 100/2)
     };
 
     window.addEventListener('resize', onWindowResize, false);
