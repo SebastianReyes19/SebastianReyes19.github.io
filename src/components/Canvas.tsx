be38739672ee9d9d3b9d3c0232e5bad3cb664cc3
SebastianReyes <prof.reyes.sebastian50@gmail.com>
2023-01-28 19:35:04 -0600

    spinning rings effect!

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -15,10 +15,17 @@ function Canvas(){
     var planet = new THREE.SphereGeometry(1.5, 20, 10);
     var little = new THREE.SphereGeometry(.375, 10, 10);
 
+    var circle = new THREE.RingGeometry(6.5, 7, 32, 1, 0, Math.PI * 2);
+    var innerC = new THREE.RingGeometry(5, 5.5);
+    
     var mat = new THREE.MeshNormalMaterial({flatShading: true});
+    var dynamMat = new THREE.MeshBasicMaterial();
     
     var sphere = new THREE.Mesh(planet, mat);
     var moon = new THREE.Mesh(little, mat);
+
+    var orbit = new THREE.Mesh(circle, dynamMat);
+    var innerO = new THREE.Mesh(innerC, dynamMat);
 
     moon.position.x += 3;
 
@@ -28,19 +35,28 @@ function Canvas(){
     group.add(moon);
 
     scene.add(group);
+    scene.add(orbit);
+    scene.add(innerO);
 
     group.rotation.set(0, 0, tilt);
-
-    camera.position.z = 5;
+    camera.position.z = 7.5;
 
     var animation = function (){
         setTimeout(function(){
             requestAnimationFrame(animation);
+
             group.rotateY(.1);
             sphere.rotateY(.5);
+            orbit.rotateZ(.01);
+            innerO.rotateZ(-.01);
+
             renderer.render(scene, camera);
-        }, 100/2)
+        }, 2)
     };
+
+    setInterval(() => {
+        dynamMat.color.setStyle('#' + Math.random().toString(16).slice(-6));
+    }, 2000);
 
     window.addEventListener('resize', onWindowResize, false);
     function onWindowResize(){
