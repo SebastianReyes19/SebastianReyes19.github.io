d98ff7986ccb5522182a472e7a49358cd5914124
SebastianReyes <prof.reyes.sebastian50@gmail.com>
2023-02-22 23:08:48 -0600

    removed buttons.tsx, added reactive ui for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.scss';
 import Header from './components/Header';
 import Canvas from './components/Canvas';
-import Intro from './components/Intro';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
-      <Intro/>
       <Canvas/>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,46 @@
+import React, {useState} from "react";
 import "./Header.scss"
-import Button from './Button';
+import "./Button.scss"
+import Intro from './Intro';
 
 function Header(){
     const str = ['Home', 'About Me', 'Projects','Contact']
+
+    const [activeComp, setActiveComp] = useState('Home');
+
+    function buttonClick(comp: string){
+        switch (comp) {
+            case str[0]:
+                setActiveComp(comp);
+                break;
+            case str[1]:
+                setActiveComp(comp);
+                break;
+            case str[2]:
+                setActiveComp(comp);
+                break;
+            case str[3]:
+                setActiveComp(comp);
+                break
+            default:
+                setActiveComp(str[0]);
+                break;
+        }
+    }
 
     return(  
         <>
             <div className={"header"}>
                 {str.map((item) =>{
                     return <div className="header-content">
-                        <Button text={item} className='Header-Button'></Button>
+                        <button className="Header-Button" id={item + "-btn"} onClick={() => buttonClick(item)}>
+                            {item}
+                        </button>
                     </div>
                 })
                 }
             </div>
+            {activeComp === str[0] ? <Intro/> : null}
         </>
     )
 }
