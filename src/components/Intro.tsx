278c89fd4e7b967f0faf5f19c373df4078e52248
SebastianReyes <prof.reyes.sebastian50@gmail.com>
2023-02-23 15:13:50 -0600

    Made major changes to UI

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,25 +3,25 @@ import portrait from '../portrait.jpg'
 
 function AboutMe(){
     return(
-        <div id='introContent'>
-        <div className='intro Statement'>
-            <h1>
-                Welcome.
-            </h1>
-            <p>
-                My name is Sebastian Reyes
-                <br/>
-                a Computer Science Undergraduate at Texas A&M University.
-                <br/>
-                I currently specialize in web, software, and 
-                <br/>
-                game development and computer graphics.
+        <div className='intro'>
+            <div className='intro Statement'>
+                <h1>
+                    Welcome.
+                </h1>
+                <p>
+                    My name is Sebastian Reyes
+                    <br/>
+                    a Computer Science Undergraduate at Texas A&M University.
+                    <br/>
+                    I currently specialize in web, software, and 
+                    <br/>
+                    game development and computer graphics.
 
-            </p>
-        </div>
-        <div className='intro Photo'>
-            <img src={portrait} alt="Sebastian Reyes"/>
-        </div>
+                </p>
+            </div>
+            <div className='intro Photo'>
+                <img src={portrait} alt="Sebastian Reyes"/>
+            </div>
         </div>
     )
 }
